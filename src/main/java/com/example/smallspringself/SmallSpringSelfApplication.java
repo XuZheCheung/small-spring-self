bb0f2982fa84716f1b24815d17305ac1455c0072
Xuzhe Cheung <joneicu@163.com>
2023-10-17 21:45:54 +0800

    change build tool

---
+++ src/main/java/com/example/smallspringself/SmallSpringSelfApplication.java
@@ -0,0 +1,13 @@
+package com.example.smallspringself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallSpringSelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallSpringSelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smallspringself/SmallSpringSelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smallspringself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallSpringSelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
